76074c3063763b42916aa67de4be90b919d34dac
Matvey-1212 <matvey_antonov@mail.ru>
2024-12-09 01:00:16 +0400

    HW10

--- 10/src/capi_utils/setup.py
+++ 10/src/capi_utils/setup.py
@@ -1,13 +1,18 @@
+"""
+Сборщик си файлов
+"""
 from setuptools import setup, Extension
 
 
 def run():
+    """ 
+    run
+    """
     setup(
         name="custom_json",
         version="1.0.0",
         ext_modules=[Extension("custom_json", ["custom_json.c"])],
     )
-
 
 
 if __name__ == "__main__":
